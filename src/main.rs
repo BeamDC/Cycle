c449421b54e60ad6dac8b1ec8e28a3d0b787fd57
Bin_Laptop <ethan.ramdeen64@gmail.com>
2026-01-17 15:07:06 -0500

    bevy main added, will work on actually implementing gameplay soon

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() { println!("hello world"); }
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: bevy::window::PresentMode::Immediate,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
+}
